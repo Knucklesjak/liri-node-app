3c5e8018bccb83cb14efd34a02e2b3ce5c61cc41
Jason Kunkel <jasonakunkel@outlook.com>
2018-02-12 14:58:27 -0800

    started readme, created keys.js, random.txt, liri.js and json files

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'c4uxs4ufaUDAGR4gBVOF2a4hG',
+  consumer_secret: '1twdNi0f4odIJYS4mMQ2dXfxKle9pVPc1lWNH43FNtvn7dtlar',
+  access_token_key: '794200046-ZpWCgz4gri99S68VJlETMqZo3fLV3dCfkXrvwMgW',
+  access_token_secret: 'xzDnmWOkFiL4GhiVKcnqacPQsAYhgpZE8pa7si2SrMMbG',
+}
+
+module.exports = twitterKeys;
