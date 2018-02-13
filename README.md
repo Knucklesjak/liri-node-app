fafebcba2003e7f04bc95e90b2ff45a3465cb828
Jason Kunkel <jasonakunkel@outlook.com>
2018-02-13 15:35:18 -0800

    twitter function call. no spotify or movie call yet

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# liri-node-app
+<h1>LIRI</h1>
+
+### This is an introduction to node.js repo for a UC Berkeley Coding Boot Camp. I am using node.js and JS to execute command line function outside of a browser. 
+
+LIRI executes four commands:
+	1) my-tweets
+	2) spotify-this-song
+	3) movie-this
+	4) do-what-it-says 
---
+++ liri.js
@@ -0,0 +1,27 @@
+// Node modules needed for functions to work
+var fs = require('fs');
+var request = require("request");
+var twitter = require("twitter");
+var keys = require('./keys.js');
+
+console.log(keys)
+console.log(keys.consumer_key)
+
+// Variable to call the instance of twitter and to call to node modules and to keys.js to protect info.
+var client = new twitter({
+			consumer_key: keys.consumer_key,
+  			consumer_secret: keys.consumer_secret,
+  			access_token_key: keys.access_token_key,
+  			access_token_secret: keys.access_token_secret		
+  		});
+
+// Parameters for twitter function
+var params = {screen_name: "knucklesjak", count: 20};
+
+
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
+		
