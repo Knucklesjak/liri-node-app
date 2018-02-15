f2b14c2daa0bb3ba04ea12da92efc66e457164c2
Jason Kunkel <jasonakunkel@outlook.com>
2018-02-14 19:53:56 -0800

    lost functionality of node command. Won't fire

--- liri.js
+++ liri.js
@@ -42,55 +42,64 @@ var secondCommand = process.argv[3];
 
 
 // Variable to call the instance of twitter and to call to node modules and to keys.js to protect info.
-var client = new Twitter({
-			consumer_key: keys.twitterKeys.consumer_key,
-  			consumer_secret: keys.twitterKeys.consumer_secret,
-  			access_token_key: keys.twitterKeys.access_token_key,
-  			access_token_secret: keys.twitterKeys.access_token_secret		
-  		});
-if(process.argv[2] === "my-tweets"){
-// Parameters for twitter function
-var params = {screen_name: "knucklesjak", count: 20};
-e
+function getTweets(){
+	console.log("Get ready for my tweets!");
+	var client = new Twitter({
+				consumer_key: keys.twitterKeys.consumer_key,
+	  			consumer_secret: keys.twitterKeys.consumer_secret,
+	  			access_token_key: keys.twitterKeys.access_token_key,
+	  			access_token_secret: keys.twitterKeys.access_token_secret		
+	  		});
+
+// Parameters for twitter function, calling on last 20 tweets
+	var params = {screen_name: "knucklesjak", count: 20};
+
 // Calling on twitter what info to pull from tweet history
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-  	console.log("-----------------------");
-  	for (var i = 0; i < tweets.length; i++) {
-  	console.log("------------------------");
-    console.log(tweets[i].created_at);
-    console.log(tweets[i].text);
-	}
-  }
-}); 
-} 
-
-else if (process.argv[2] === 'spotify-this-song'){
-var song = process.argv[3]
-if(song === undefined){
-	song = "All the small things"
-}
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	  if (!error) {
+	  	console.log("-----------------------");
+	  	for (var i = 0; i < tweets.length; i++) {
+	  	console.log("------------------------");
+	    console.log(tweets[i].created_at);
+	    console.log(tweets[i].text);
+		}
+	  };
+	}); 
+}; /// end of getTweets function
 
 
-// Variable to call on the instance of spotify
-var spotify = new Spotify({
-  id: keys.spotifyKeys.client_id,
-  secret: keys.spotifyKeys.client_secret
-});
- 
-spotify.search({ type: 'track', query: song }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
- //  	// var chicken = data.tracks.items
- // 	for (var i = 0; i < chicken.length; i++) {
- // 		console.log(chicken[i].album.name);
- // 		console.log(chicken[i].album.href);
- // 	}
-	// console.log(data); 	
-	console.log(data.tracks.items[0].album.name) //album name
-	console.log(data.tracks.items[0].album.artists[0].name)
-	console.log(data.tracks.items[0].album.href)
-	console.log(song)
-});
+
+// else if (process.argv[2] === 'spotify-this-song'){
+// var song = process.argv[3]
+// if(song === undefined){
+// 	song = "All the small things"
+// }
+
+
+// Function for spotify
+function mySpotify() {
+	console.log("Check out this song!");
+
+	// Variable to call on the instance of spotify and set default of chosen song undefined
+	var spotify = new Spotify({
+	  id: keys.spotifyKeys.client_id,
+	  secret: keys.spotifyKeys.client_secret
+	});
+
+	var searchMusic;
+		if(secondCommand === undefined) {
+			searchMusic = "I Want it That Way";
+		}
+
+	 // starts the spotify search
+	spotify.search({ type: 'track', query: song }, function(err, data) {
+	  if (err) {
+	    return console.log('Error occurred: ' + err);
+	  }
+		
+		console.log(data.tracks.items[0].album.name) //album name
+		console.log(data.tracks.items[0].album.artists[0].name)
+		console.log(data.tracks.items[0].album.href)
+		console.log(song)
+	});
 }
